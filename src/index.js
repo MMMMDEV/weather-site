7cead7b623b029596822e67c0846385d7ab7140d
mario <manriqueduran123@gmail.com>
2022-11-30 13:43:48 -0600

    css for links

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { latLon } from "./functions/getData";
 
 import "./normalize.css";
 import "./style.css";
+import "./styleLinks.css";
 
 displayTop(latLon("nashville"));
 displayBottom(latLon("nashville"));
