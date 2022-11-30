6b608bbb37350d1c992881a9de73ada0521c933e
mario <manriqueduran123@gmail.com>
2022-11-30 13:46:46 -0600

    temporary fix

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import { latLon } from "./functions/getData";
 
 import "./normalize.css";
 import "./style.css";
-import "./styleLinks.css";
 
 displayTop(latLon("nashville"));
 displayBottom(latLon("nashville"));
