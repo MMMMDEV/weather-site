5620c76fa6d79ecaa9aa107845f519d40fa04f53
mario <manriqueduran123@gmail.com>
2022-11-29 20:39:29 -0600

    test

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import { displayBottom, displayTop } from "./functions/dom";
 import { latLon } from "./functions/getData";
 
+import clouds from "./images/clouds.png";
+
 import "./normalize.css";
 import "./style.css";
-import "./images";
-import "./links.html";
 
 displayTop(latLon("nashville"));
 displayBottom(latLon("nashville"));
